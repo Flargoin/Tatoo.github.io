0f37472f4a70ad26fbe130f459a5bbe916cd4fc3
Flargoin <54916933+Flargoin@users.noreply.github.com>
2020-04-07 23:45:35 +0300

    Add files via upload

---
+++ active.js
@@ -0,0 +1,10 @@
+
+  $(function () { 
+    $('.nav-menu a').each(function () {
+    var location = window.location.href;
+    var link = this.href; 
+    if(location == link) {
+    $(this).addClass('active');
+    }
+    });
+    });
